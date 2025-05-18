dd0f607f1cf75a06028ccdf7df50de3c6b76cdb2
ANIL CHAUDHARY <87251166+anilchaudhary449@users.noreply.github.com>
2025-05-18 13:17:38 +0000

    commit

--- Encapsulations/Human_Main.java
+++ Encapsulations/Human_Main.java
@@ -3,14 +3,14 @@ class Human{
  private int age;
   private String name; 
   
-  public void setAge(int a){
-    age=a;
+  public void setAge(int age){
+    this.age=age;
   }
   public int getAge(){
     return age;
   }
-  public void setName(String n){ // here, "Anil" assigned to n then to name
-    name=n;
+  public void setName(String name){ // here, "Anil" assigned to n then to name
+    this.name=name;
   }
   public String getName(){
     return name;
@@ -20,7 +20,7 @@ public class Human_Main {
     public static void main(String[] args) {
     Human obj=new Human();
     obj.setAge(23);
-    obj.setName("Anil");
+    obj.setName("Anil");;
 
     System.out.println(obj.getAge()+":"+obj.getName());
 }
